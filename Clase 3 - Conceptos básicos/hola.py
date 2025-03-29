29355b7d53c672ba5a54b2e045e26c4aebf19d2b
Edson J Vargas <edson.jhair@yahoo.com>
2025-03-28 23:15:51 -0500

    Hasta clase 8

---
+++ Clase 3 - Conceptos básicos/hola.py
@@ -0,0 +1,6 @@
+saludo_1="Hola"
+saludo_2="hi"
+nombre="Edson"
+
+print(saludo_1)
+print(nombre)
---
+++ Clase 8 - Operaciones matemáticas/index.py
@@ -0,0 +1,39 @@
+a = 10
+b = 3
+print("Suma:", a + b)
+print("Resta:", a - b)
+print("Multiplicación:", a * b)
+print("División:", a / b)
+print("Módulo", a % b)
+print("Parte entera de la división", a // b)
+print("Potenciación", a**b)
+a = a + 2
+a += 2
+print(a)
+a -= 2
+print(a)
+a *= 2
+print(a)
+a /= 2
+print(a)
+a //= 2
+print(a)
+a %= 2
+print(a)
+# La jerarquía de operaciones es la siguiente:
+# 1. Paréntesis
+# 2. Potenciación
+# 3. Multiplicación y división
+# 4. Suma y resta
+# En caso de que haya operaciones de igual jerarquía, se evalúan de izquierda a derecha
+
+c = 10.0
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+print(a == b)
+print(a <= c)
+print(a != b)
+# Operadores de comparación
+# Operadores lógicos
