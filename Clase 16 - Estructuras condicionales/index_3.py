38eed9a73b8c2fc2fe5790809e907f1590560c37
Edson J Vargas <edson.jhair@yahoo.com>
2025-03-30 21:26:47 -0500

    Clase 16

---
+++ Clase 16 - Estructuras condicionales/index_3.py
@@ -0,0 +1,10 @@
+is_member = True
+age = 15
+
+if is_member:
+    if age >= 15:
+        print("Tienes acceso ya que eres miembro y eres mayor o tienes 15 años")
+    else:
+        print("No tienes acceso ya que eres miembro pero menor a 15 años")
+else:
+    print("No eres miembro y no tienes acceso")
