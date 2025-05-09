0e380636f9960311649157c2f32a9aeefa0c041a
Edson J Vargas <edson.jhair@yahoo.com>
2025-05-09 14:21:39 -0500

    Clase 59

---
+++ Clase 59 - Módulos y paquetes: Reutilización y Organización de código/app.py
@@ -0,0 +1,7 @@
+import reports
+
+sales_report = reports.generate_sales_report("October", 10000)
+expenses_report = reports.generate_expenses_report("October", 5000)
+
+print(sales_report)
+print(expenses_report)
---
+++ Clase 59 - Módulos y paquetes: Reutilización y Organización de código/reports.py
@@ -0,0 +1,6 @@
+def generate_sales_report(month, sales):
+    return f"Sales Report - {month}: Total sales: {sales}"
+
+
+def generate_expenses_report(month, expenses):
+    return f"Expenses Report - {month}: Total expenses: {expenses}"
